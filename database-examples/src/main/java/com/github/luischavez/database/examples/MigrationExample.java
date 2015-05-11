5f4529f6c21d623f68abb1c933906046ac79df6e
luischavez <luis_13_30_4@hotmail.com>
2015-05-10 19:33:31 -0600

    updated license header.

--- database-examples/src/main/java/com/github/luischavez/database/examples/MigrationExample.java
+++ database-examples/src/main/java/com/github/luischavez/database/examples/MigrationExample.java
@@ -1,5 +1,5 @@
-/*
- * Copyright (C) 2015 Luis Chávez {@literal <https://github.com/luischavez>}
+/* 
+ * Copyright (C) 2015 Luis Chávez
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
